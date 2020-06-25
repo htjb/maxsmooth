e855ac03a3c27926b13ab5b1918c8e7743308c52
htjb <harrytjbevins@yahoo.co.uk>
2020-06-25 10:25:29 +0100

    additional tests added

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pytest
 from maxsmooth.DCP import smooth
 from numpy.testing import assert_almost_equal
 
@@ -15,7 +16,23 @@ def test_api():
     sol = smooth(x, y, N)
     # Check RMS/Chi/y_fit calculated/returned correctly
     assert_almost_equal(sol.rms, np.sqrt(np.sum((y - sol.y_fit)**2)/len(y)))
-    assert_almost_equal( sol.Optimum_chi , ((y - sol.y_fit)**2).sum())
+    assert_almost_equal( sol.optimum_chi , ((y - sol.y_fit)**2).sum())
+
+def test_keywords():
+    np.random.seed(0)
+
+    Ndat = 100
+    x = np.linspace(0,1,Ndat)
+    y = 1 + x + x**2 + x**3
+
+    N = 4
+
+    with pytest.raises(Exception):
+        sol = smooth(x, y, N, color='pink')
+
+    with pytest.raises(Exception):
+        sol = smooth(x, y, N, base_dir='file')
+
 
 def test_smooth():
     # Check parameters of smooth function are correct
@@ -24,9 +41,8 @@ def test_smooth():
     Ndat = 100
     x = np.linspace(0,1,Ndat)
     y = 1 + x + x**2 + x**3
-    #y = x**2
 
     N = 4
     sol = smooth(x, y, N, model_type='polynomial')
 
-    assert_almost_equal(sol.Optimum_params.T[0], [1, 1, 1, 1], decimal=3)
+    assert_almost_equal(sol.optimum_params.T[0], [1, 1, 1, 1], decimal=3)
