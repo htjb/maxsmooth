65cb84e426f4ff739546e0cc362d6599a7674817
htjb <harrytjbevins@yahoo.co.uk>
2025-11-04 13:02:44 +0000

    function to generate functions to evaluate derivatives up to m

--- maxsmooth/models.py
+++ maxsmooth/models.py
@@ -2,43 +2,44 @@ from jax import numpy as jnp
 import jax
 
 @jax.jit
-def normalised_polynomial(x, y, pivot_point, params):
+def normalised_polynomial(x, norm_x, norm_y, params):
     i = jnp.arange(params.shape[0])
-    powers = (x / x[pivot_point])[None, :] ** i[:, None]  
-    y_sum = y[pivot_point] * jnp.sum(params[:, None] * powers, axis=0)
+    powers = (x / norm_x) ** i 
+    y_sum = norm_y * jnp.sum(params * powers, axis=0)
     return y_sum
 
 @jax.jit
-def polynomial(x, y, pivot_point, params):
+def polynomial(x, norm_x, norm_y, params):
     i = jnp.arange(params.shape[0])
-    powers = x[None, :] ** i[:, None]
-    y_sum = jnp.sum(params[:, None] * powers, axis=0)
+    powers = x ** i
+    y_sum = jnp.sum(params * powers, axis=0)
     return y_sum
 
 @jax.jit
-def loglog_polynomial(x, y, pivot_point, params):
+def loglog_polynomial(x, norm_x, norm_y, params):
     i = jnp.arange(params.shape[0])
-    powers = jnp.log10(x)[None, :] ** i[:, None]
-    y_sum = 10**jnp.sum(params[:, None] * powers, axis=0)
+    powers = jnp.log10(x) ** i
+    y_sum = 10**jnp.sum(params * powers, axis=0)
     return y_sum
 
 @jax.jit
-def exponential(x, y, pivot_point, params):
+def exponential(x, norm_x, norm_y, params):
     i = jnp.arange(params.shape[0])
-    powers = jnp.exp(-i[:, None] * x[None, :] / x[pivot_point])
-    y_sum = y[pivot_point] * jnp.sum(params[:, None] * powers, axis=0)
+    x = x + 1e-10  # avoid division by zero
+    powers = jnp.exp(-i * x / norm_x)
+    y_sum = norm_y * jnp.sum(params * powers, axis=0)
     return y_sum
 
 @jax.jit
-def log_polynomial(x, y, pivot_point, params):
+def log_polynomial(x, norm_x, norm_y, params):
     i = jnp.arange(params.shape[0])
-    powers = jnp.log10(x / x[pivot_point])[None, :] ** i[:, None]
-    y_sum = jnp.sum(params[:, None] * powers, axis=0)
+    powers = jnp.log10(x / norm_x) ** i
+    y_sum = jnp.sum(params * powers, axis=0)
     return y_sum
 
 @jax.jit
-def difference_polynomial(x, y, pivot_point, params):
+def difference_polynomial(x, norm_x, norm_y, params):
     i = jnp.arange(params.shape[0])
-    powers = (x - x[pivot_point])[None, :] ** i[:, None]
-    y_sum = jnp.sum(params[:, None] * powers, axis=0)
+    powers = (x - norm_x) ** i
+    y_sum = jnp.sum(params * powers, axis=0)
     return y_sum
